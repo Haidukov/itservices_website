e48db6eeaab8871b675eb200443986ba112881b8
rostyslav-haidukov <rostyslav.haidukov@live.com>
2023-05-06 18:06:26 +0300

    redesign

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react';
+import { css } from '@emotion/react';
 import {
   ArrowPathIcon,
   Bars3Icon,
@@ -11,19 +12,41 @@ import {
 } from '@heroicons/react/24/outline';
 import Logo from '../images/logo.svg';
 
+const menuItemCss = css`
+  font-size: 16px;
+  font-weight: 500;
+  font-family: sans-serif;
+  color: #0e1071;
+`;
+
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white">
+    <header
+      className="bg-white"
+      css={css`
+        position: sticky;
+        top: 120px;
+        transform: translateX(20px);
+        background: rgba(255, 255, 255, 0.5);
+        backdrop-filter: blur(50px);
+        border-radius: 60px;
+      `}
+    >
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-7xl h-16 items-center p-1 lg:px-8"
         aria-label="Global"
       >
-        <div className="flex lg:flex-1">
+        <div
+          className="flex"
+          css={css`
+            margin-right: 60px;
+          `}
+        >
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img className="h-16 w-auto" src={Logo} alt="" />
+            <img className="h-12 w-auto" src={Logo} alt="" />
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -37,73 +60,17 @@ export default function Header() {
           </button>
         </div>
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
-          <a href="#" className="text-lg font-semibold leading-6 text-gray-900">
+          <a href="#" className="text-lg leading-6" css={menuItemCss}>
             Features
           </a>
-          <a href="#" className="text-lg font-semibold leading-6 text-gray-900">
+          <a href="#" className="text-lg leading-6" css={menuItemCss}>
             Marketplace
           </a>
-          <a href="#" className="text-lg font-semibold leading-6 text-gray-900">
+          <a href="#" className="text-lg leading-6" css={menuItemCss}>
             Company
           </a>
         </Popover.Group>
       </nav>
-      <Dialog
-        as="div"
-        className="lg:hidden"
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-      >
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img className="h-12 w-auto" src={Logo} alt="" />
-            </a>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-12 w-12" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Features
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Marketplace
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Company
-                </a>
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
-              </div>
-            </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
     </header>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import { css } from '@emotion/react';
 import Header from '../components/Header';
 
 const IndexPage = () => {
   return (
     <main>
       <Header />
+      <div
+        css={css`
+          margin: 20px;
+          background-color: #f6efff;
+          width: 100%;
+          border-radius: 45px;
+          font-family: sans-serif;
+          padding: 90px 80px;
+        `}
+      >
+        <h1
+          css={css`
+            font-size: 56px;
+            font-weight: 700;
+            background: linear-gradient(104.1deg, #7735e8, #ba2df0, #c62cf1);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+          `}
+        >
+          Everything you need to build in Web3
+        </h1>
+        <p
+          css={css`
+            color: #5a6893;
+            line-height: 1.5;
+            font-size: 20px;
+          `}
+        >
+          Build & scale your project with our comprehensive set of Web3 tools.
+          <br />
+          We provide wallet services, marketplace solutions, NFT tools, and
+          more.
+        </p>
+        <button
+          css={css`
+            margin-top: 20px;
+            background: linear-gradient(
+              104.1deg,
+              #7735e8 32.26%,
+              #ba2df0 80.02%,
+              #c62cf1 88.17%
+            );
+            color: white;
+            border-color: #0e1071;
+            font-size: 18px;
+            font-weight: 600;
+            transition: #0f0644 0.5s ease, #420084 0.5s ease;
+            width: 215px;
+            height: 59px;
+            border-radius: 60px;
+          `}
+        >
+          View our services
+        </button>
+      </div>
     </main>
   );
 };
